2d105d46d5ffa4b35e64f4f70bf819ea7168ffd7
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2022-06-29 14:10:46 +0530

    Basic loop question

---
+++ assignment10.c
@@ -0,0 +1,162 @@
+/*
+// Solution:1)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<n;i++)
+    printf("MySirG ");
+    return 0;
+}*/
+/*Solution:2)
+// 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}*/
+
+/*
+Solution:3)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    printf("%d ",n+1-i);
+    return 0;
+}*/
+
+/*
+// Solution:4)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    printf("%d ",2*i);
+    return 0;
+}*/
+/*
+// Solution:5)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d "2*(n+1)-2*i);
+    }
+    return 0;
+}*/
+
+/*
+// Solution:6)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d ",2*i-1);
+
+    }
+return 0;
+}*/
+
+/*
+// Solution:7)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+    printf("%d ",i*i);
+    }
+    return 0;
+}*/
+
+/*
+// Solution:8)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+    printf("%d ",i*i*i);
+    }
+    return 0;
+}*/
+/*
+// Solution:9)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=n;i>=1;i--)
+    {
+    printf("%d ",i*i);
+    }
+    return 0;
+}*/
+
+/*
+// Solution:10)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n;
+printf("Enter value of n");
+scanf("%d",&n);
+    for(int i=n;i>=1;i--)
+    {
+    printf("%d ",i*i*i);
+    }
+    return 0;
+}*/
+
+
+
+/*
+C Language Community LIVE Classes
+Assignment-10
+
+
+Write a program to print MySirG N times on the screen.
+Write a program to print the first N natural numbers.
+Write a program to print the first N natural numbers in reverse order.
+Write a program to print the first N even natural numbers.
+Write a program to print the first N even natural numbers in reverse order.
+Write a program to print the first N odd natural numbers 
+Write a program to print the squares of first N natural numbers
+Write a program to print the cubes of first N natural numbers
+Write a program to print the squares of first N natural numbers in reverse order
+Write a program to print the cubes of the first N natural numbers in reverse order.
+
+
+*/

26e94cf557d6098a6e6bfff4bbbb60cdd616caf1
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2022-06-29 14:13:06 +0530

    Basic for loop question solution

---
+++ assignment9.c
@@ -0,0 +1,121 @@
+/*
+// Solution:1)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<10;i++)
+    printf("MySirG ");
+    return 0;
+}*/
+/*Solution:2)
+// 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}*/
+
+/*
+Solution:3)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    printf("%d ",11-i);
+    return 0;
+}*/
+
+/*
+// Solution:4)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    printf("%d ",2*i);
+    return 0;
+}*/
+/*
+// Solution:5)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d "22-2*i);
+    }
+    return 0;
+}*/
+
+/*
+// Solution:6)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d ",2*i-1);
+
+    }
+return 0;
+}*/
+
+/*
+// Solution:7)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+    printf("%d ",i*i);
+    }
+    return 0;
+}*/
+
+/*
+// Solution:8)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+    printf("%d ",i*i*i);
+    }
+    return 0;
+}*/
+/*
+// Solution:9)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=10;i>=1;i--)
+    {
+    printf("%d ",i*i);
+    }
+    return 0;
+}*/
+
+/*
+// Solution:10)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=10;i>=1;i--)
+    {
+    printf("%d ",i*i*i);
+    }
+    return 0;
+}*/
